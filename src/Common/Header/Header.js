bd561c469605e33be9b3804d6396010323284e49
Dark Matter <amanpathak8184@gmail.com>
2021-08-31 21:45:44 +0530

    Added Validation for Login Form

--- src/Common/Header/Header.js
+++ src/Common/Header/Header.js
@@ -5,11 +5,16 @@ import './Header.css';
 import logo from '../../Assets/logo.svg';
 
 // Material UI Imports
-import { Tab, Typography, Button, Tabs, FormControl, InputLabel, Input } from '@material-ui/core';
+import { Tab, Typography, Button, Tabs, FormControl, InputLabel, Input, FormHelperText } from '@material-ui/core';
 
 // Other Imports
 
 import Modal from 'react-modal';
+
+import PropTypes from 'prop-types';
+
+
+
 
 
 const customStyles = {
@@ -23,24 +28,29 @@ const customStyles = {
     }
 }
 
+
+//#region  Tab Container
+
 const TabStyle = {
     padding: 0
-
 }
 
 const TabContainer = function (props) {
     return (
         <div>
-
             <Typography component="div" style={TabStyle} align='center'>
-
                 {props.children}
-
             </Typography>
-
         </div>
     )
 }
+
+TabContainer.propTypes = {
+    children: PropTypes.node.isRequired
+}
+
+//#endregion
+
 
 class Header extends Component {
 
@@ -48,7 +58,9 @@ class Header extends Component {
         super();
         this.state = {
             isModalOpen: false,
-            value: 0
+            value: 0,
+            username: "",
+            userNameRequired: "displayNone"
         };
     }
 
@@ -65,18 +77,29 @@ class Header extends Component {
     }
 
     tabChangeHandler = (event, newValue) => {
-
         this.setState({
             value: newValue
         })
+    }
 
-        console.log("New Value IS " + newValue);
-        console.log("State Value IS " + this.state.value);
+    loginClickHandler = () => {
+        this.state.username === "" ?
+            this.setState({ userNameRequired: "displayBlock" }) : this.setState({ userNameRequired: "displayNone" })
+    }
+
+    inputUsernameChangeHandler = (e) => {
+        this.setState({
+            username: e.target.value
+        })
     }
 
     render() {
+
         return (
+
+
             <div className="Header">
+
 
                 <img src={logo} className="Logo" alt="logo" />
 
@@ -99,26 +122,58 @@ class Header extends Component {
                         <Tab label="Register"></Tab>
                     </Tabs>
 
-                    <TabContainer>
+                    {this.state.value === 0 &&
+                        <TabContainer>
 
-                        <FormControl required>
-                            <InputLabel htmlFor="userName"> Username </InputLabel>
-                            <Input id="username" type='text' />
+                            <FormControl required>
+                                
+                                <InputLabel htmlFor="userName"> Username </InputLabel>
 
-                        </FormControl>
+                                <Input id="username" type='text' className={this.state.username}
+                                    onChange={this.inputUsernameChangeHandler} />
 
-                        <br />
-                        <br />
+                                <FormHelperText className={this.state.userNameRequired}>
+                                    <span className="red"> Required </span>
+                                </FormHelperText>
 
-                        <FormControl required>
-                            <InputLabel htmlFor="password"> Password </InputLabel>
-                            <Input id="password" type='password' />
+                            </FormControl> <br /><br />
 
-                        </FormControl> <br /> <br />
+                            <FormControl required>
+                                <InputLabel htmlFor="password"> Password </InputLabel>
+                                <Input id="password" type='password' />
 
-                        <Button variant="contained" color="primary" type='submit'> Login</Button>
+                            </FormControl> <br /> <br />
 
-                    </TabContainer>
+                            <Button variant="contained" color="primary" type='submit' onClick={this.loginClickHandler}>
+                                Login
+                            </Button>
+
+                        </TabContainer>
+                    }
+
+
+                    {this.state.value === 1 &&
+                        <TabContainer>
+
+                            <FormControl required>
+                                <InputLabel htmlFor="name"> Name </InputLabel>
+                                <Input id="name" type='text' />
+
+                            </FormControl>
+
+                            <br />
+                            <br />
+
+                            <FormControl required>
+                                <InputLabel htmlFor="Email"> Email </InputLabel>
+                                <Input id="Email" type='email' />
+
+                            </FormControl> <br /> <br />
+
+                            <Button variant="contained" color="primary" type='submit'> Register</Button>
+
+                        </TabContainer>
+                    }
 
                 </Modal>
 
